3ce98a99fac4c931924ebfffe78f8fe30850baef
grturner <grturner@5x5code.com>
2018-04-12 12:33:07 -0400

    User registration working with Core3

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  get 'users/index'
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  
 end
