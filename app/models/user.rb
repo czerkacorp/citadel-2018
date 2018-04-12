eeea86cb5d4ecae15b9f3e3c5041acaca1508ff8
grturner <grturner@5x5code.com>
2018-04-12 17:49:19 -0400

    Added uniquness constraint on username

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
-  self.table_name = "accounts"
+  self.table_name = 'accounts'
+  validates :username, uniqueness: true
   attribute :admin_level, :integer, :limit => 1
 end
