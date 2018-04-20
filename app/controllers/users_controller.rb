cc5d0eb63e266986da419ab7b2410c345399ad4f
grturner <grturner@5x5code.com>
2018-04-19 23:36:40 -0400

    Initial user/edit.html.erb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,6 +14,10 @@ class UsersController < ApplicationController
     redirect_to users_path
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
   def show
     @user = User.find(params[:id])
   end
