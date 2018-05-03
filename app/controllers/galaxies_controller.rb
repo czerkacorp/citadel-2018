04b4ca22e10210ae9973a8678825c25b0e4a2d51
James McLean <jmclean@czerka.io>
2018-05-03 16:42:01 -0400

    Added galaxy routes and basic controller definitions

--- app/controllers/galaxies_controller.rb
+++ app/controllers/galaxies_controller.rb
@@ -1,2 +1,21 @@
 class GalaxiesController < ApplicationController
+
+    def index
+        @galaxies = Galaxy.all
+      end
+    
+      def create
+      end
+    
+      def edit
+        @user = Galaxy.find(params[:id])
+      end
+    
+      def update
+      end  
+    
+      def show
+        @user = Galaxy.find(params[:id])
+      end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   resources :users
 
+
   namespace :admin do
     resources :settings
+    resources :galaxies
   end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
