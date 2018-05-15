84b41ab547abfb25f44c343d9a78ceca37bc7554
James McLean <jmclean@czerka.io>
2018-05-15 14:50:37 -0400

    Commented out the debugging put statements in application_helper.rb

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,20 +4,20 @@ module ApplicationHelper
         themesList = []
         
         #The puts statements are for debugging the inner workings of the themes list search.
-        puts "== Initial List Array ---> " + initialList.inspect
-        puts "============================================================="
-        puts "== Searching for themes in app/assets/stylesheets/themes ===="
-        puts "============================================================="
+        #puts "== Initial List Array ---> " + initialList.inspect
+        #puts "============================================================="
+        #puts "== Searching for themes in app/assets/stylesheets/themes ===="
+        #puts "============================================================="
             initialList.each do |t|
                 t = t.sub(".scss", "")
                 puts "== Theme " + t + " detected!"
                 themesList.push(t)
             end
-        puts "============================================================="
-        puts "== Finished searching themes ================================"
-        puts "============================================================="
+        #puts "============================================================="
+        #puts "== Finished searching themes ================================"
+        #puts "============================================================="
         themesList.sort!
-        puts "== Themes List Array ---> " + themesList.inspect
+        #puts "== Themes List Array ---> " + themesList.inspect
         return themesList
     end
 end
