7066b5db8da0b2b839573ca166eff8e0829f34e5
James McLean <jmclean@czerka.io>
2018-05-15 15:16:18 -0400

    Minor adjustments to application_helper"

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,6 @@ module ApplicationHelper
     def themesList
         initialList = Dir.entries(Rails.root.join("app/assets/stylesheets/themes")).reject! { |x| ( x === ".") || ( x === "..") }.sort
         themesList = []
-        
         #The puts statements are for debugging the inner workings of the themes list search.
         #puts "== Initial List Array ---> " + initialList.inspect
         #puts "============================================================="
@@ -10,7 +9,7 @@ module ApplicationHelper
         #puts "============================================================="
             initialList.each do |t|
                 t = t.sub(".scss", "")
-                puts "== Theme " + t + " detected!"
+                #puts "== Theme " + t + " detected!"
                 themesList.push(t)
             end
         #puts "============================================================="
@@ -20,4 +19,5 @@ module ApplicationHelper
         #puts "== Themes List Array ---> " + themesList.inspect
         return themesList
     end
+    
 end
